25936c195745873f6491c8549bd100ff99ff271b
kaungmyat128 <40614801@live.napier.ac.uk>
2023-07-10 16:17:53 +0630

    Added Country.java

---
+++ src/main/java/com/Ace/sem/Country.java
@@ -0,0 +1,79 @@
+package com.Ace.sem;
+
+public class Country {
+    public static void main(String[] args) {
+
+    }
+    /**
+     * City Code
+     */
+    public String Code;
+    /**
+     * City Name
+     */
+    public String Name;
+
+    /**
+     * City Continent
+     */
+    public String Continent;
+
+    /**
+     * City Region
+     */
+    public String Region;
+
+    /**
+     * City SurfaceArea
+     */
+    public float SurfaceArea;
+    /**
+     * City IndepYear
+     */
+    public int IndepYear;
+
+    /**
+     * City Population
+     */
+    public int Population;
+
+    /**
+     * City LifeExpectancy
+     */
+    public float LifeExpectancy;
+
+    /**
+     * City GNP
+     */
+    public float GNP;
+
+    /**
+     * City GNPOld
+     */
+    public float GNPOld;
+
+    /**
+     * City LocalName
+     */
+    public String LocalName;
+    /**
+     * City GovermentForm
+     */
+    public String GovermentForm;
+
+    /**
+     * City HeadOfState
+     */
+    public String HeadOfState;
+
+    /**
+     * City Capital
+     */
+    public int Capital;
+    /**
+     * City Code2
+     */
+    public String Code2;
+
+
+}
