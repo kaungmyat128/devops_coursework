5508baf46c9f59a67faf4e1234aeedfe7fee0eba
40614802-Zwe-Htut-Htay-Lwin <40614802@live.napier.ac.uk Country>
2023-07-10 13:03:26 +0630

    adding basic code to Cities_Population.java

--- src/main/java/com/Ace/sem/Cities_Population.java
+++ src/main/java/com/Ace/sem/Cities_Population.java
@@ -1,4 +1,31 @@
 package com.Ace.sem;
 
 public class Cities_Population {
+    {
+        /**
+         * City ID
+         */
+        public int ID;
+
+        /**
+         * City's name
+         */
+        public String Name;
+
+        /**
+         * Country's Code
+         */
+        public int CountryCode;
+
+        /**
+         * District Name
+         */
+        public String District;
+
+        /**
+         * City's Population
+         */
+        public int Population;
+
+    }
 }
