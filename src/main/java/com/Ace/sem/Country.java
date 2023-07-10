0c6bbd76922dde988dd0df4115bd9ae39a03ae28
Kaung Khant Hein <40614799@live.napier.ac.uk>
2023-07-10 20:36:54 +0630

    country reports initialized

--- src/main/java/com/Ace/sem/Country.java
+++ src/main/java/com/Ace/sem/Country.java
@@ -1,5 +1,8 @@
 package com.Ace.sem;
 
+import java.sql.*;
+import java.util.*;
+
 public class Country {
     public static void main(String[] args) {
 
@@ -75,5 +78,121 @@ public class Country {
      */
     public String Code2;
 
+    //creating an array to gather data from country table
+    public ArrayList<Country> getCountryPop(Connection con) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, "
+                            +"Population, Name "
+                            + "FROM country "
+                            + "ORDER BY Population DESC LIMIT 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Country> Countrypop = new ArrayList<Country>();
+            while (rset.next()) {
+                Country cp = new Country();
+                cp.Code = rset.getString("country.Code");
+                cp.Name = rset.getString("country.Name");
+                cp.Continent = rset.getString("country.Continent");
+                cp.Region = rset.getString("country.Region");
+                cp.Population = rset.getInt("country.Population");
+                //cp. = rset.getString("city.Name");
+                Countrypop.add(cp);
+            }
+            return Countrypop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+
+    public ArrayList<Country> getCounContPop(Connection con) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, "
+                            +"Population, Name "
+                            + "FROM country "
+                            + "WHERE Continent = "
+                            + "ORDER BY Population DESC LIMIT 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Country> Countrypop = new ArrayList<Country>();
+            while (rset.next()) {
+                Country cp = new Country();
+                cp.Code = rset.getString("country.Code");
+                cp.Name = rset.getString("country.Name");
+                cp.Continent = rset.getString("country.Continent");
+                cp.Region = rset.getString("country.Region");
+                cp.Population = rset.getInt("country.Population");
+                //cp. = rset.getString("city.Name");
+                Countrypop.add(cp);
+            }
+            return Countrypop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+    public ArrayList<Country> getCounRegPop(Connection con) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, "
+                            +"Population, Name "
+                            + "FROM country "
+                            + "WHERE Region = "
+                            + "ORDER BY Population DESC LIMIT 10";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Country> Countrypop = new ArrayList<Country>();
+            while (rset.next()) {
+                Country cp = new Country();
+                cp.Code = rset.getString("country.Code");
+                cp.Name = rset.getString("country.Name");
+                cp.Continent = rset.getString("country.Continent");
+                cp.Region = rset.getString("country.Region");
+                cp.Population = rset.getInt("country.Population");
+                //cp. = rset.getString("city.Name");
+                Countrypop.add(cp);
+            }
+            return Countrypop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
+
+    public void displayInfo(ArrayList<Country> countrypop)
+    {
+        // Print header
+        System.out.println("============================================================");
+        System.out.println(String.format("%-10s %-15s %-15s %-15s %-15s", "Code", "Name", "Continent", "Region", "Population"));
+        // Loop over all employees in the list
+        for (Country cp : countrypop)
+        {
+            String world_info =
+                    String.format("%-10s %-15s %-15s %-15s %-15s",
+                            cp.Code, cp.Name, cp.Continent, cp.Region, cp.Population);
+            System.out.println(world_info);
+        }
+        System.out.println("============================================================");
+    }
 
 }
