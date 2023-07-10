be3c863d7686b86e751e1b806d34e24e124f1231
kaungmyat128 <40614801@live.napier.ac.uk>
2023-07-10 14:20:32 +0630

    Initial world population test

--- src/main/java/com/Ace/sem/App.java
+++ src/main/java/com/Ace/sem/App.java
@@ -1,16 +1,25 @@
 package com.Ace.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public String Name;
+    public int Population;
+
+    public static void main(String[] args) {
         // Create new Application
         App a = new App();
 
         // Connect to database
         a.connect();
+
+        // Extract employee salary information
+        ArrayList<App> World = a.getWorldPop(10);
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(World.size());
+        a.displayInfo(World);
 
         // Disconnect from database
         a.disconnect();
@@ -24,39 +33,29 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -65,19 +64,54 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public ArrayList<App> getWorldPop(int num) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Name,Population "
+                            + "FROM city "
+                            + "ORDER BY Population DESC LIMIT num";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<App> Worldpop = new ArrayList<App>();
+            while (rset.next()) {
+                App world = new App();
+                world.Name = rset.getString("city.Name");
+                world.Population = rset.getInt("city.Population");
+                Worldpop.add(world);
+            }
+            return Worldpop;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+    public void displayInfo(ArrayList<App> World)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s", "Name", "Population"));
+        // Loop over all employees in the list
+        for (App world : World)
+        {
+            String world_info =
+                    String.format("%-10s %-15s",
+                            world.Name, world.Population);
+            System.out.println(world_info);
         }
     }
 }
