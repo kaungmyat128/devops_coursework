533da4505817a341fd3dc3d29c519f6126963aaa
40614802-Zwe-Htut-Htay-Lwin <40614802@live.napier.ac.uk Country>
2023-07-11 12:48:37 +0630

    adding code in city_report.java

--- src/main/java/com/Ace/sem/City_report.java
+++ src/main/java/com/Ace/sem/City_report.java
@@ -12,8 +12,8 @@ public class City_report extends City {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT city.Name,coutnry.Country,District,Population "
-                            + "FROM city "
+                    "SELECT (city.Name) AS CityName,country.Name AS CountryName,city.District,city.Population "
+                            + "FROM city INNER JOIN country ON city.CountryCode = country.Code "
                             + "ORDER BY Population DESC LIMIT 5";
             // Execute SQL statement
             ResultSet data1 = stmt.executeQuery(strSelect);
@@ -21,8 +21,8 @@ public class City_report extends City {
             ArrayList<City> City_report = new ArrayList<City>();
             while (data1.next()) {
                 City ct = new City();
-                ct.Name = data1.getString("city.Name");
-                ct.Country = data1.getString("country.name");
+                ct.CityName = data1.getString("city.Name");
+                ct.CountryName = data1.getString("CountryName");
                 ct.District = data1.getString("city.District");
                 ct.Population = data1.getInt("city.Population");
                 City_report.add(ct);
