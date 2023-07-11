b03a8a67d3263c84b476630d48c6d2d26cc7adb7
kaungmyat128 <40614801@live.napier.ac.uk>
2023-07-11 15:20:26 +0630

    third attempt on correcting the error

--- src/main/java/com/Ace/sem/TopPopulatedCities.java
+++ src/main/java/com/Ace/sem/TopPopulatedCities.java
@@ -14,7 +14,7 @@ public class TopPopulatedCities {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-            "SELECT city.Name AS CityName, country.Name AS CountryName, country.District, city.Population "
+            "SELECT city.Name AS CityName, country.Name AS CountryName, city.District, city.Population "
                     + "FROM country LEFT JOIN city ON country.Code = city.CountryCode "
                     + "ORDER BY Population DESC LIMIT 20";
             // Execute SQL statement
@@ -25,7 +25,7 @@ public class TopPopulatedCities {
                 City world = new City();
                 world.CityName = rset.getString("CityName");
                 world.CountryName = rset.getString("CountryName");
-                world.District = rset.getString("country.District");
+                world.District = rset.getString("city.District");
                 world.Population = rset.getInt("city.Population");
                 worldPop.add(world);
             }
@@ -43,9 +43,9 @@ public class TopPopulatedCities {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, country.District, country.Continent, city.Population "
+                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, city.District, country.Continent, city.Population "
                             + "FROM country LEFT JOIN city ON country.Code = city.CountryCode) AS subquery "
-                            + "WHERE row_num <= 20 ORDER BY Continent ASC, Population DESC";
+                            + "WHERE row_num <= 20 ORDER BY Population DESC LIMIT 20";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract employee information
@@ -54,7 +54,7 @@ public class TopPopulatedCities {
                 City continent = new City();
                 continent.CityName = rset.getString("CityName");
                 continent.CountryName = rset.getString("CountryName");
-                continent.District = rset.getString("country.District");
+                continent.District = rset.getString("city.District");
                 continent.Continents = rset.getString("country.Continent");
                 continent.Population = rset.getInt("city.Population");
                 continentPop.add(continent);
@@ -73,9 +73,9 @@ public class TopPopulatedCities {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, country.District, country.Region, city.Population "
+                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, city.District, country.Region, city.Population "
                             + "FROM country LEFT JOIN city ON country.Code = city.CountryCode) AS subquery "
-                            + "WHERE row_num <= 20 ORDER BY Region ASC, Population DESC";
+                            + "WHERE row_num <= 20 ORDER BY Population DESC LIMIT 20";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract employee information
@@ -84,7 +84,7 @@ public class TopPopulatedCities {
                 City region = new City();
                 region.CityName = rset.getString("CityName");
                 region.CountryName = rset.getString("CountryName");
-                region.District = rset.getString("country.District");
+                region.District = rset.getString("city.District");
                 region.Region = rset.getString("country.Region");
                 region.Population = rset.getInt("city.Population");
                 regionPop.add(region);
@@ -103,9 +103,9 @@ public class TopPopulatedCities {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, country.District, city.Population "
+                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, city.District, city.Population "
                             + "FROM country LEFT JOIN city ON country.Code = city.CountryCode) AS subquery "
-                            + "WHERE row_num <= 20 ORDER BY CountryName ASC, Population DESC";
+                            + "WHERE row_num <= 20 ORDER BY Population DESC LIMIT 20";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract employee information
@@ -114,7 +114,7 @@ public class TopPopulatedCities {
                 City country = new City();
                 country.CityName = rset.getString("CityName");
                 country.CountryName = rset.getString("CountryName");
-                country.District = rset.getString("country.District");
+                country.District = rset.getString("city.District");
                 country.Population = rset.getInt("city.Population");
                 countryPop.add(country);
             }
@@ -132,9 +132,9 @@ public class TopPopulatedCities {
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, country.District, city.Population "
+                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY city.Population DESC) AS row_num, city.Name AS CityName, country.Name AS CountryName, city.District, city.Population "
                             + "FROM country LEFT JOIN city ON country.Code = city.CountryCode) AS subquery "
-                            + "WHERE row_num <= 20 ORDER BY District ASC, Population DESC";
+                            + "WHERE row_num <= 20 ORDER BY Population DESC LIMIT 20";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Extract employee information
@@ -143,7 +143,7 @@ public class TopPopulatedCities {
                 City district = new City();
                 district.CityName = rset.getString("CityName");
                 district.CountryName = rset.getString("CountryName");
-                district.District = rset.getString("country.District");
+                district.District = rset.getString("city.District");
                 district.Population = rset.getInt("city.Population");
                 districtPop.add(district);
             }
