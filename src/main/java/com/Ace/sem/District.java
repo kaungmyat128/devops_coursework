8acbd2be181057b3c98110f15570eb3d9b87f362
40501814-Hein Thaw Zin <40501814@live.napier.ac.uk>
2023-07-11 12:01:16 +0630

    added District.

---
+++ src/main/java/com/Ace/sem/District.java
@@ -0,0 +1,37 @@
+package com.Ace.sem;
+
+import java.sql.*;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class District {
+    public static void main(String[] args) {
+
+    }
+    /**
+     * City ID
+     */
+    public int ID;
+    /**
+     * City Name
+     */
+    public String Name;
+
+    /**
+     * City CountryCode
+     */
+    public String CountryCode;
+
+    /**
+     * City District
+     */
+    public String District;
+
+    /**
+     * City Population
+     */
+    public int Population;
+}
