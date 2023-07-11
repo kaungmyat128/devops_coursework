2bc0fba6f742ab77c7fc89e16758082e52d082b6
40501814-Hein Thaw Zin <40501814@live.napier.ac.uk>
2023-07-11 14:16:25 +0630

    updated District but have some errors.

--- src/main/java/com/Ace/sem/App.java
+++ src/main/java/com/Ace/sem/App.java
@@ -25,7 +25,8 @@ public class App {
         TopPopulatedCities cty = new TopPopulatedCities();
         // Create new object for top countries population Class Java
         top_countries_population c2 = new top_countries_population();
-
+        // Create new object for district Class Java
+        District dis = new District();
         // Create new object for City_report java Class
         City_report cityR = new City_report();
 
@@ -68,6 +69,11 @@ public class App {
 
         ArrayList<City> CtyTop = cty.getCityWorldPop(a.con);
         cty.displayTopCities(CtyTop);
+
+        // Display district population based on
+        ArrayList<City> DisPop = cityR.get_city_world(a.con);
+        System.out.println("All districts sorted by population in the world ");
+        dis.displayDistrict(DisPop);
 
         // Disconnect from database
         a.disconnect();
--- src/main/java/com/Ace/sem/District.java
+++ src/main/java/com/Ace/sem/District.java
@@ -1,10 +1,5 @@
 package com.Ace.sem;
 
-import java.sql.*;
-import java.util.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 
 public class District {
@@ -12,26 +7,110 @@ public class District {
 
     }
     /**
-     * City ID
+     * City Code
      */
-    public int ID;
+    public int Code;
     /**
-     * City Name
-     */
-    public String Name;
-
-    /**
-     * City CountryCode
-     */
-    public String CountryCode;
-
-    /**
-     * City District
+     * District Name
      */
     public String District;
+
+    /**
+     * Region
+     */
+    public String Region;
+
+    /**
+     * City Continent
+     */
+    public String Continent;
 
     /**
      * City Population
      */
     public int Population;
+
+    /**
+     * City Capital
+     */
+    public int Capital;
+}
+    //display all districts
+    public void displayDistrict(ArrayList<City> districts_list)
+    {
+        // Print header
+        System.out.println("============================================================");
+        System.out.println("All districts sorted by population in the world ");
+
+        System.out.println(String.format("%-10s %-40s %-15s %-20s %-15s %-15s", "Code", "District", "Continent", "Region", "Population", "Capital City"));
+        // Loop over all district population in the list
+        for (District dis : districts_list)
+        {
+            String districts_info =
+                    String.format("%-10s %-40s %-15s %-20s %-15s %-15s",
+                            dis.Code, dis.District, dis.Continent, dis.Region, dis.Population, dis.Capital);
+            System.out.println(districts_info);
+        }
+        System.out.println("============================================================");
+    }
+
+    //display regions based on continents
+    public void displayDistrict_Continent(ArrayList<District> districts_list)
+    {
+        // Print header
+        System.out.println("============================================================");
+
+        // Initialize Current Continent variable
+        String currentContinent = null;
+
+        // Loop over all countries population in the list
+        for (District dis : districts_list)
+        {
+            // Check the current continent changed or not
+            if (!dis.Continent.equals(currentContinent)) {
+                // Print the continent header
+                System.out.println("\n District sorted by Population in " + dis.Continent + " Continent");
+                System.out.println("===========================================");
+                currentContinent = dis.Continent;
+                System.out.println(String.format("%-10s %-40s %-15s %-20s %-15s %-15s", "Code", "District", "Continent", "Region", "Population", "Capital City"));
+
+            }
+
+            String districts_info =
+                    String.format("%-10s %-40s %-15s %-20s %-15s %-15s",
+                            dis.Code, dis.District, dis.Continent, dis.Region, dis.Population, dis.Capital);
+            System.out.println(districts_info);
+        }
+        System.out.println("============================================================");
+    }
+
+    //display districts based on region
+    public void displayDistrict_Region(ArrayList<District> districts_list)
+    {
+        // Print header
+        System.out.println("============================================================");
+
+        // Initialize Current Region variable
+        String currentRegion = null;
+
+        // Loop over all countries population in the list
+        for (District dis : districts_list)
+        {
+            // Check the current continent changed or not
+            if (!dis.Region.equals(currentRegion)) {
+                // Print the continent header
+                System.out.println("\n Districts sorted by Population in " + cp.Region + " Region");
+                System.out.println("===========================================");
+                currentRegion = dis.Region;
+                System.out.println(String.format("%-10s %-40s %-15s %-20s %-15s %-15s", "Code", "District", "Continent", "Region", "Population", "Capital City"));
+
+            }
+
+            String districts_info =
+                    String.format("%-10s %-40s %-15s %-20s %-15s %-15s",
+                            dis.Code, dis.District, dis.Continent, dis.Region, dis.Population, dis.Capital);
+            System.out.println(districts_info);
+        }
+        System.out.println("============================================================");
+    }
 }
