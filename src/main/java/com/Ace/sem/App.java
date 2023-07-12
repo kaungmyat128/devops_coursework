f1dd62407f60dbac1b18afd33c6b4b901d48f129
Kaung Khant Hein <40614799@live.napier.ac.uk>
2023-07-12 15:16:38 +0630

    If statements applied, testing accuracy

--- src/main/java/com/Ace/sem/App.java
+++ src/main/java/com/Ace/sem/App.java
@@ -39,35 +39,36 @@ public class App {
         //---------------All Country Report---------------
 
         // Display All Countries Population
-        ArrayList<Country> CPop1 = c2.get_top_countries(a.con, 0);
+        ArrayList<Country> CPop1 = cr.get_countries(a.con, 0);
         System.out.println("All Countries Population in the World");
         c.displayCountries(CPop1);
 
-//        // Display All Countries Population based on Each Continent
-//        ArrayList<Country> CPop2 = cr.getCounContPop(a.con);
-//        System.out.println("All Countries Population in the World categorized by Continents");
-//        c.displayCountries_Continent(CPop2);
-//
-//        // Display All Countries Population based on Each Region
-//        ArrayList<Country> CPop3 = cr.getCounRegPop(a.con);
-//        System.out.println("All Countries Population in the World categorized by Regions");
-//        c.displayCountries_Region(CPop3);
+        // Display All Countries Population based on Each Continent
+        ArrayList<Country> CPop2 = cr.get_countries_cont(a.con, 0);
+        System.out.println("All Countries Population in the World categorized by Continents");
+        c.displayCountries_Continent(CPop2);
+
+        // Display All Countries Population based on Each Region
+        ArrayList<Country> CPop3 = cr.get_countries_region(a.con, 0);
+        System.out.println("All Countries Population in the World categorized by Regions");
+        c.displayCountries_Region(CPop3);
 
         //---------------Top Country Report---------------
 
         // Display Top 10 Countries Population
-        ArrayList<Country> CPop4 = c2.get_top_countries(a.con, 10);
+        ArrayList<Country> CPop4 = cr.get_countries(a.con, 10);
         System.out.println("Top 10 Countries Population in the World");
         c.displayCountries(CPop4);
-//        // Display Top 10 Countries Population in Each Continent
-//        ArrayList<Country> CPop5 = c2.get_top_countries_continents(a.con);
-//        System.out.println("Top 10 Countries Population in Each Continent");
-//        c.displayCountries_Continent(CPop5);
-//
-//        // Display Top 10 Countries Population in Each Region
-//        ArrayList<Country> CPop6 = c2.get_top_countries_regions(a.con);
-//        System.out.println("Top 10 Countries Population in Each Region");
-//        c.displayCountries_Region(CPop6);
+
+        // Display Top 10 Countries Population in Each Continent
+        ArrayList<Country> CPop5 = cr.get_countries_cont(a.con, 10);
+        System.out.println("Top 10 Countries Population in Each Continent");
+        c.displayCountries_Continent(CPop5);
+
+        // Display Top 10 Countries Population in Each Region
+        ArrayList<Country> CPop6 = cr.get_countries_region(a.con,10);
+        System.out.println("Top 10 Countries Population in Each Region");
+        c.displayCountries_Region(CPop6);
 //
 //        //---------------All Cities Report---------------
 //
--- src/main/java/com/Ace/sem/CountryReport.java
+++ src/main/java/com/Ace/sem/CountryReport.java
@@ -11,64 +11,66 @@ import java.util.ArrayList;
 public class CountryReport {
 
     //creating an array to gather data from country table
-    public ArrayList<Country> getCountryPop(Connection con) {
+    public ArrayList<Country> get_countries (Connection con, int lim){
         try {
+
             // Create an SQL statement
             Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT country.Code, country.Name, country.Continent, country.Region, "
-                            +"country.Population, city.Name AS Capital "
-                            + "FROM country JOIN city on city.ID = country.Capital "
-                            + "ORDER BY Population DESC";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Extract employee information
-            ArrayList<Country> Countrypop = new ArrayList<>();
-            while (rset.next()) {
-                Country cp = new Country();
-                cp.setCode(rset.getString("Code"));
-                cp.setName(rset.getString("Name"));
-                cp.setContinent(rset.getString("Continent"));
-                cp.setRegion(rset.getString("Region"));
-                cp.setPopulation(rset.getInt("Population"));
-                cp.setCapital(rset.getString("Capital"));
-                Countrypop.add(cp);
+            String strSelect = null;
+            if (lim>0) {
+                // Create string for SQL statement
+                strSelect =
+                        "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as Capital "
+                                + "FROM country INNER JOIN city ON country.Capital = city.ID "
+                                + "ORDER BY country.Population DESC LIMIT " + lim;
+            } else if (lim==0) {
+                // Create string for SQL statement
+                strSelect =
+                        "SELECT country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as Capital "
+                                + "FROM country INNER JOIN city ON country.Capital = city.ID "
+                                + "ORDER BY country.Population DESC";
             }
-            return Countrypop;
-        } catch (Exception e) {
+            // Execute SQL statement
+            ResultSet query1 = stmt.executeQuery(strSelect);
+            //creates array to gather country data based on population
+            ArrayList<Country> top_countries_population = new ArrayList<>();
+            return store_into_arraylist(top_countries_population, query1);
+
+        }
+        // Exception handling when any errors occur. Print out error type and error message and return null.
+        catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get country population report");
+            System.out.println("Failed to return countries population around the world");
             return null;
         }
     }
 
     //creating an array of countries based on continents with descending order based on population
-    public ArrayList<Country> getCounContPop(Connection con) {
+    public ArrayList<Country> get_countries_cont(Connection con, int lim) {
         try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
+            String strSelect = null;
             // Create string for SQL statement
-            String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY country.Population DESC) AS row_num, "
-                            + "country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name AS Capital FROM country "
-                            + "INNER JOIN city ON country.Capital = city.ID) AS subquery "
-                            + "ORDER BY Continent ASC, Population DESC";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Extract employee information
-            ArrayList<Country> Countrypop = new ArrayList<>();
-            while (rset.next()) {
-                Country cp = new Country();
-                cp.setCode(rset.getString("Code"));
-                cp.setName(rset.getString("Name"));
-                cp.setContinent(rset.getString("Continent"));
-                cp.setRegion(rset.getString("Region"));
-                cp.setPopulation(rset.getInt("Population"));
-                cp.setCapital(rset.getString("Capital"));
-                Countrypop.add(cp);
+            if (lim>0) {
+                strSelect =
+                        "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY country.Population DESC) AS row_num, "
+                                + "country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name AS Capital FROM country "
+                                + "INNER JOIN city ON country.Capital = city.ID) AS subquery "
+                                + "ORDER BY Continent ASC, Population DESC";
+            } else if (lim==0) {
+                strSelect =
+                        "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Continent ORDER BY country.Population DESC) AS row_num, "
+                                + "country.Code, country.Name, country.Continent, country.Region, country.Population, city.Name as Capital FROM country "
+                                + "INNER JOIN city ON country.Capital = city.ID) AS subquery WHERE row_num <= " + lim
+                                + " ORDER BY Continent ASC, Population DESC";
             }
-            return Countrypop;
+            // Execute SQL statement
+            ResultSet query2 = stmt.executeQuery(strSelect);
+            // Creates array to gather populated country data based on each continent
+            ArrayList<Country> top_countries_population = new ArrayList<>();
+            return store_into_arraylist(top_countries_population, query2);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get country population report");
@@ -77,31 +79,33 @@ public class CountryReport {
     }
 
     //creating an array of countries based on region with descending order based on population
-    public ArrayList<Country> getCounRegPop(Connection con) {
+    public ArrayList<Country> get_countries_region(Connection con, int lim) {
         try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Region ORDER BY country.Population DESC) AS row_num, "
-                            + "country.Code, country.Name, country.Continent, country.Region, country.Population, "
-                            + "city.Name as Capital FROM country LEFT JOIN city ON country.Capital = city.ID) AS subquery "
-                            + "ORDER BY Region ASC, Population DESC";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Extract employee information
-            ArrayList<Country> Countrypop = new ArrayList<>();
-            while (rset.next()) {
-                Country cp = new Country();
-                cp.setCode(rset.getString("Code"));
-                cp.setName(rset.getString("Name"));
-                cp.setContinent(rset.getString("Continent"));
-                cp.setRegion(rset.getString("Region"));
-                cp.setPopulation(rset.getInt("Population"));
-                cp.setCapital(rset.getString("Capital"));
-                Countrypop.add(cp);
+
+                // Create an SQL statement
+                Statement stmt = con.createStatement();
+                String strSelect = null;
+                // Create string for SQL statement
+            if (lim>0) {
+                strSelect =
+                        "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Region ORDER BY country.Population DESC) AS row_num, "
+                                + "country.Code, country.Name, country.Continent, country.Region, country.Population, "
+                                + "city.Name as Capital FROM country LEFT JOIN city ON country.Capital = city.ID) AS subquery "
+                                + "ORDER BY Region ASC, Population DESC";
+            } else if (lim==0) {
+                strSelect =
+                        "SELECT * FROM (SELECT ROW_NUMBER() OVER (PARTITION BY country.Region ORDER BY country.Population DESC) AS row_num, "
+                                + "country.Code, country.Name, country.Continent, country.Region, country.Population, "
+                                + "city.Name as Capital FROM country LEFT JOIN city ON country.Capital = city.ID) AS subquery "
+                                + "WHERE row_num <= " + lim + " ORDER BY Region ASC, Population DESC";
             }
-            return Countrypop;
+
+
+            // Execute SQL statement
+            ResultSet query3 = stmt.executeQuery(strSelect);
+            // Creates array to gather top 20 populated country data based on each continent
+            ArrayList<Country> top_countries_population = new ArrayList<>();
+            return store_into_arraylist(top_countries_population, query3);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Failed to get country population report");
@@ -109,4 +113,27 @@ public class CountryReport {
         }
     }
 
+
+        public ArrayList<Country> store_into_arraylist(ArrayList<Country> al, ResultSet qry) {
+            try{
+                // Extract population of countries information and store into array list
+                while (qry.next()) {
+                    Country cp = new Country();
+                    cp.setCode(qry.getString("Code"));
+                    cp.setName(qry.getString("Name"));
+                    cp.setContinent(qry.getString("Continent"));
+                    cp.setRegion(qry.getString("Region"));
+                    cp.setPopulation(qry.getInt("Population"));
+                    cp.setCapital(qry.getString("Capital"));
+                    al.add(cp);
+                }
+                // return the array list
+                return al;
+            }// Exception handling when any errors occur. Print out error type and error message and return null.
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("Failed to return countries population in each continent");
+                return null;
+            }
+        }
 }
