1c5ee6ad0b113ab9da29aa0afad7b485aa57d502
may <40614797@live.napier.ac.uk>
2023-07-21 15:00:25 +0630

    Integrated testing error fixed

--- src/main/java/com/Ace/sem/App.java
+++ src/main/java/com/Ace/sem/App.java
@@ -236,15 +236,15 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world", "root", "example");
+                //con = DriverManager.getConnection("jdbc:mysql://db:3306/world", "root", "example");
                 // Connect to database in localhost
                 // con = DriverManager.getConnection("jdbc:mysql://localhost:33061/world", "root", "example");
 
-//                if (i%2 == 0) {
-//                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world", "root", "example");
-//                } else{
-//                    con = DriverManager.getConnection("jdbc:mysql://localhost:33061/world", "root", "example");
-//                }
+                if (i%2 == 0) {
+                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world", "root", "example");
+                } else{
+                    con = DriverManager.getConnection("jdbc:mysql://localhost:33060/world", "root", "example");
+                }
                System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
